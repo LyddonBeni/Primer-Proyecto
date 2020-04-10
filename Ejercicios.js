39f197c4bcac1cfafef69966a34dd42eed06e9ca
Lyddon Beni Varje Esteban <LyddonBeni@gmail.com>
2020-04-09 23:57:59 -0500

    Actualizando

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,13 +1,14 @@
-
-alert("Hola mundo");
-alert("Estamos aprendiendo programación básica");
-alert("Usando GitHub y Atom");
-var nombre="LyddonBeni";
-alert("Mi nombre es "+nombre)
-nombre = prompt("Cual es tu nombre")
-alert("Hola "+nombre)
-alert("Probemos un ejercicio")
-var x=1;
-var y=2;
-var z=x+y;
-alert("El Valor de z es "+z)
+// Primera parte
+  alert("Hola mundo");
+  alert("Estamos aprendiendo programación básica");
+  alert("Usando GitHub y Atom");
+  var nombre="LyddonBeni";
+  alert("Mi nombre es "+nombre)
+  nombre = prompt("Cual es tu nombre")
+// Segunda Parte
+  alert("Hola "+nombre)
+  alert("Probemos un ejercicio")
+  var x=1;
+  var y=2;
+  var z=x+y;
+  alert("El Valor de z es "+z)
