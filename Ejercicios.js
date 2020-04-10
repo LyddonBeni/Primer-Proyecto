d9c63c98e5ef70fa0b9ea794aa2c4608eb3f6c1a
Lyddon Beni Varje Esteban <LyddonBeni@gmail.com>
2020-04-09 22:24:29 -0500

    Programacion con platzi
    Siguiendo el curos de programacion basica en Platzi

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,3 +1,4 @@
+
 alert("Hola mundo");
 alert("Estamos aprendiendo programación básica");
 alert("Usando GitHub y Atom");
@@ -5,3 +6,8 @@ var nombre="LyddonBeni";
 alert("Mi nombre es "+nombre)
 nombre = prompt("Cual es tu nombre")
 alert("Hola "+nombre)
+alert("Probemos un ejercicio")
+var x=1;
+var y=2;
+var z=x+y;
+alert("El Valor de z es "+z)
