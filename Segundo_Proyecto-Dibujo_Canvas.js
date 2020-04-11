5730df69a927aa8037519a48224b05d3dda379ef
Lyddon Beni Varje Esteban <LyddonBeni@gmail.com>
2020-04-10 19:12:28 -0500

    Dibujando Ccanvas

--- Segundo_Proyecto-Dibujo_Canvas.js
+++ Segundo_Proyecto-Dibujo_Canvas.js
@@ -1,2 +1,22 @@
 var p=document.getElementById("Plano");
 var lienzo=p.getContext("2d");
+console.log (lienzo);
+
+lienzo.beginPath();
+// asigna el color con strokeStyle
+lienzo.strokeStyle = "blue";
+// con moveTo nos movemos a una pocision especifica
+lienzo.moveTo(0,0);
+// creamos una linea con lineTo
+lienzo.lineTo(500,500);
+// dibujamos la linea con stroke
+lienzo.stroke();
+// con closePath cerramos el trazo
+lienzo.closePath();
+
+lienzo.beginPath();
+lienzo.strokeStyle = "red";
+lienzo.moveTo(10, 100);
+lienzo.lineTo(200, 10);
+lienzo.stroke();
+lienzo.closePath();
